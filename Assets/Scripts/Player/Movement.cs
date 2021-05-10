77c7e4f4acbe55395f6ce94d397c5a14a9353b1c
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2021-05-10 22:01:46 +0500

    10.05.21
    +Background
    +Basic UI sprites
    +Player
    +Basic player movements & animations

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Animator Animator;
+    public bool Grounded;
+
+    private Vector3 directionVector = new Vector3();
+    private float inputAxis;
+    private Vector3 currentScale;
+
+    void Update()
+    {
+        if (Grounded && Input.GetButtonDown("Jump"))
+            Jump();
+
+        inputAxis = Input.GetAxis("Horizontal");
+        Animator.SetFloat("Horizontal", inputAxis);
+        directionVector.x = inputAxis;
+        if (inputAxis > 0 && transform.localScale.x < 0 || inputAxis < 0 && transform.localScale.x > 0)
+            DoSpriteMirroring();
+
+        inputAxis = Input.GetAxis("Vertical");
+        Animator.SetFloat("Vertical", inputAxis);
+        directionVector.y = inputAxis;
+
+        transform.position += directionVector * Math.Abs(gameObject.transform.localScale.x) * Time.deltaTime;
+    }
+
+    private void Jump() => gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+
+    private void DoSpriteMirroring()
+    {
+        currentScale = transform.localScale;
+        currentScale.Set(currentScale.x * -1, currentScale.y, currentScale.z);
+        transform.localScale = currentScale;
+    }
+}
