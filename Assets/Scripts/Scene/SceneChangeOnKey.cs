756810f23a8fd90cfecc3c4644ee835e0f335295
Misso25 <goldengoo@mail.ru>
2021-06-02 01:21:35 +0500

    02.06.2021
    Что-то типо скрипта для перемещения из папки в уровень

---
+++ Assets/Scripts/Scene/SceneChangeOnKey.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeOnKey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            if (Input.GetKeyDown(KeyCode.PageDown) || Input.GetKeyDown(KeyCode.S))
+                SceneManager.LoadScene(0);
+    }
+}
