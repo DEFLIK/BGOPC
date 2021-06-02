e5bea0add4a922bf4bffc5a1620ffe7b4d1f42b5
idetestmyself <n.vdovin.ne@gmail.com>
2021-06-02 14:34:12 +0500

    Revert "02.06.2021"
    This reverts commit 9a56c33b53abcb9100fa0728c822a4ee4c8a6bc1.

--- Assets/Scripts/Assistant/AssitantTrigger.cs
+++ Assets/Scripts/Assistant/AssitantTrigger.cs
@@ -17,10 +17,11 @@ public class AssitantTrigger : MonoBehaviour
     private void Start()
     {
         pointer.SetActive(false);
-        assistant.SetActive(true);
-        isActive = true;
+        assistant.SetActive(false);
+        isActive = false;
         assAnim = assistant.GetComponent<Animator>();
         pointAnim = pointer.GetComponent<Animator>();
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -34,6 +35,36 @@ public class AssitantTrigger : MonoBehaviour
         }
     }
 
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.E))
+    //        {
+    //            assistant.SetActive(true);
+
+    //            assAnim.Play("Appearance");
+    //            isActive = true;
+    //        }
+    //    }
+
+    //    if (Input.GetButtonDown("f"))
+    //    {
+    //        if (!isActive)
+    //        {
+    //            assistant.SetActive(true);
+
+    //            assAnim.Play("Appearance");
+    //            isActive = true;
+    //        }
+    //        else
+    //        {
+    //            assAnim.Play("Disappearance");
+    //            isActive = false;
+    //        }
+    //    }
+    //}
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -41,6 +72,22 @@ public class AssitantTrigger : MonoBehaviour
             pointAnim.Play("PointerDisappear");
             //assAnim.Play("Disappearance");
         }
+    }
+
+    IEnumerator Disappearance()
+    {
+        assAnim.Play("Disappearance");
+        yield return new WaitForSeconds(1f);
+        assistant.SetActive(false);
+        isActive = false;
+    }
+
+    IEnumerator PointerDisappear()
+    {
+        pointAnim.Play("PointerDisappear");
+        yield return new WaitForSeconds(1f);
+        pointer.SetActive(false);
+
     }
 
     private void Update()
