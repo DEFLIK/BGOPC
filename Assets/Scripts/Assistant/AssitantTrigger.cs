ff9796e50bab26c3c99955edaaab77ac4695467a
idetestmyself <n.vdovin.ne@gmail.com>
2021-06-02 15:00:49 +0500

    получай

--- Assets/Scripts/Assistant/AssitantTrigger.cs
+++ Assets/Scripts/Assistant/AssitantTrigger.cs
@@ -17,8 +17,8 @@ public class AssitantTrigger : MonoBehaviour
     private void Start()
     {
         pointer.SetActive(false);
-        assistant.SetActive(false);
-        isActive = false;
+        assistant.SetActive(true);
+        isActive = true;
         assAnim = assistant.GetComponent<Animator>();
         pointAnim = pointer.GetComponent<Animator>();
         
@@ -35,35 +35,7 @@ public class AssitantTrigger : MonoBehaviour
         }
     }
 
-    //private void OnTriggerStay2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag == "Player")
-    //    {
-    //        if (Input.GetKeyDown(KeyCode.E))
-    //        {
-    //            assistant.SetActive(true);
-
-    //            assAnim.Play("Appearance");
-    //            isActive = true;
-    //        }
-    //    }
-
-    //    if (Input.GetButtonDown("f"))
-    //    {
-    //        if (!isActive)
-    //        {
-    //            assistant.SetActive(true);
-
-    //            assAnim.Play("Appearance");
-    //            isActive = true;
-    //        }
-    //        else
-    //        {
-    //            assAnim.Play("Disappearance");
-    //            isActive = false;
-    //        }
-    //    }
-    //}
+   
 
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -72,22 +44,6 @@ public class AssitantTrigger : MonoBehaviour
             pointAnim.Play("PointerDisappear");
             //assAnim.Play("Disappearance");
         }
-    }
-
-    IEnumerator Disappearance()
-    {
-        assAnim.Play("Disappearance");
-        yield return new WaitForSeconds(1f);
-        assistant.SetActive(false);
-        isActive = false;
-    }
-
-    IEnumerator PointerDisappear()
-    {
-        pointAnim.Play("PointerDisappear");
-        yield return new WaitForSeconds(1f);
-        pointer.SetActive(false);
-
     }
 
     private void Update()
